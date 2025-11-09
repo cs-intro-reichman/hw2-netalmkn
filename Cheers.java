57381fd6538514569651473edf5c0094de51232a
netalmkn <netalmkn@gmail.com>
2025-11-09 10:15:37 +0200

    Completed Assignment

--- Cheers.java
+++ Cheers.java
@@ -1,22 +1,25 @@
-
 public class Cheers {
-        public static void main(String[] args) {
-	    String cheer = args[0];
-            String anLetters = "AEFHILMNORSX";
-            int numCheers = Integer.parseInt(args[1]);
-            for (int i = 0; i < cheer.length(); i++){
-                char c = cheer.charAt(i);
-                char upper = Character.toUpperCase(c);
-                if (anLetters.indexOf(upper) != -1) {
-                        System.out.println("Give me an " + upper + ": " + upper + "!");
-                }
-                else{
-                        System.out.println("Give me a  " + upper + ": " + upper + "!");
-                }
-            } 
-            for (int i = 0; i < numCheers; i++) {
-                System.out.println(cheer.toUpperCase() +"!!!");
+    public static void main(String[] args) {
+        String cheer = args[0];
+        String anLetters = "AEFHILMNORSX";
+        int numCheers = Integer.parseInt(args[1]);
 
+        for (int i = 0; i < cheer.length(); i++) {
+            char c = cheer.charAt(i);
+            char upper = Character.toUpperCase(c);
+            if (anLetters.indexOf(upper) != -1) {
+                System.out.println("Give me an " + upper + ": " + upper + "!");
+            } 
+            else {
+
+                System.out.println("Give me a  " + upper + ": " + upper + "!");
             }
         }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < numCheers; i++) {
+            System.out.println(cheer.toUpperCase() + "!!!");
+        }
+    }
 }
